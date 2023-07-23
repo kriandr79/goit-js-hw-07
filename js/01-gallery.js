9ac2d62bc404870d503fcce46017d54abbdca635
kriandr79 <127450901+kriandr79@users.noreply.github.com>
2023-07-23 15:50:16 +0300

    task 1-2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Отримаємо посилання на елемент ul
+const ulElement = document.querySelector(".gallery");
+
+ulElement.addEventListener("click", onImageClick);
+
+ulElement.innerHTML = makeGalleryMarkup(galleryItems);
+
+
+// Створюємо розмітку
+function makeGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+    )
+    .join("");
+}
+
+// Обробка кліку
+function onImageClick(event) {
+  event.preventDefault();
+
+  // Чи кліклули саме по img, а не по UL
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  // відкриваємо модальне вікно з картинкою
+  const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // слухаємо натискання клавіш,
+  window.addEventListener("keydown", onEscapePress);
+
+  // по ESC закриваємо
+  function onEscapePress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onEscapePress);
+    }
+  }
+}
+
+// Намагався винести фунції закриття по ESC назовні, але тоді ніяк не вдається передати туди instance
+// З тієї з причини створення instance неможливо винести в окрему фукнцію.
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const ulElement = document.querySelector(".gallery");
+
+ulElement.innerHTML = makeGalleryMarkup(galleryItems);
+lightboxRun();
+
+// Створюємо розмітку
+function makeGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+// Функція запуску плагіна
+function lightboxRun() {
+  var lightbox = new SimpleLightbox(".gallery a", {
+    /* options */
+    overlay: true,
+    overlayOpacity: 0.7,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+}
+
